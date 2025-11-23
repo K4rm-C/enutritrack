43ece59611ed03d2ea6ad044d59d595bf802cf86
AlfredoPerez73 <apcoco07@gmail.com>
2025-11-22 23:41:30 -0600

    Modificaciones en el script de despliegue

--- enutritrack-client/src/api/axios.jsx
+++ enutritrack-client/src/api/axios.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-// URLs base para cada microservicio (mantener igual)
+// URLs base para cada microservicio
 const API_BASE_URL_USER = "http://35.239.67.227:3001/";
 const API_BASE_URL_MEDICAL = "http://35.239.67.227:3002/";
 const API_BASE_URL_NUTRITION = "http://35.239.67.227:3003/";
@@ -10,34 +10,12 @@ const API_BASE_URL_RECOMMENDATION = "http://35.239.67.227:3006/";
 const API_BASE_URL_CITAS_MEDIAS = "http://35.239.67.227:3008/";
 const API_BASE_URL_ALERTAS = "http://35.239.67.227:3009/";
 
-// Función para convertir objeto a XML
-const objectToXml = (obj, rootName = "root") => {
-  let xml = `<?xml version="1.0" encoding="UTF-8"?><${rootName}>`;
-
-  const toXml = (obj) => {
-    for (let key in obj) {
-      if (obj.hasOwnProperty(key)) {
-        if (typeof obj[key] === "object" && obj[key] !== null) {
-          xml += `<${key}>${toXml(obj[key])}</${key}>`;
-        } else {
-          xml += `<${key}>${obj[key]}</${key}>`;
-        }
-      }
-    }
-    return "";
-  };
-
-  toXml(obj);
-  xml += `</${rootName}>`;
-  return xml;
-};
-
 const createAxiosInstance = (baseURL) => {
   const instance = axios.create({
     baseURL,
     withCredentials: true,
     headers: {
-      "Content-Type": "application/xml", // Cambiar a XML
+      "Content-Type": "application/json",
     },
   });
 
@@ -53,11 +31,6 @@ const createAxiosInstance = (baseURL) => {
         config.headers.Authorization = `Bearer ${token}`;
       }
 
-      // Convertir data de JSON a XML antes de enviar
-      if (config.data && typeof config.data === "object") {
-        config.data = objectToXml(config.data, "request");
-      }
-
       return config;
     },
     (error) => {
@@ -66,25 +39,7 @@ const createAxiosInstance = (baseURL) => {
   );
 
   instance.interceptors.response.use(
-    (response) => {
-      // Si la respuesta es XML, convertir a JSON
-      if (
-        response.headers["content-type"]?.includes("application/xml") &&
-        typeof response.data === "string"
-      ) {
-        try {
-          const parser = new DOMParser();
-          const xmlDoc = parser.parseFromString(
-            response.data,
-            "application/xml"
-          );
-          response.data = xmlToJson(xmlDoc);
-        } catch (error) {
-          console.error("Error parsing XML response:", error);
-        }
-      }
-      return response;
-    },
+    (response) => response,
     (error) => {
       if (error.response?.status === 401) {
         console.error("Error de autenticación:", error.response.data);
@@ -94,44 +49,6 @@ const createAxiosInstance = (baseURL) => {
   );
 
   return instance;
-};
-
-// Función para convertir XML a JSON
-const xmlToJson = (xml) => {
-  let obj = {};
-
-  if (xml.nodeType === 1) {
-    // element node
-    if (xml.attributes.length > 0) {
-      obj["@attributes"] = {};
-      for (let j = 0; j < xml.attributes.length; j++) {
-        const attribute = xml.attributes.item(j);
-        obj["@attributes"][attribute.nodeName] = attribute.nodeValue;
-      }
-    }
-  } else if (xml.nodeType === 3) {
-    // text node
-    obj = xml.nodeValue;
-  }
-
-  if (xml.hasChildNodes()) {
-    for (let i = 0; i < xml.childNodes.length; i++) {
-      const item = xml.childNodes.item(i);
-      const nodeName = item.nodeName;
-      if (typeof obj[nodeName] === "undefined") {
-        obj[nodeName] = xmlToJson(item);
-      } else {
-        if (typeof obj[nodeName].push === "undefined") {
-          const old = obj[nodeName];
-          obj[nodeName] = [];
-          obj[nodeName].push(old);
-        }
-        obj[nodeName].push(xmlToJson(item));
-      }
-    }
-  }
-
-  return obj;
 };
 
 export const userAPI = createAxiosInstance(API_BASE_URL_USER);
